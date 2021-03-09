6d629cf6326b53fbdbfa239b886ea8187826c08d
Dakota <piercedakota26@gmail.com>
2021-03-09 13:02:38 -0600

    built route for api with as well as adding the functionality for deletion

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const router = require('express').Router();
+
+var noteData = JSON.parse(data);
+
+router.get('./api/notes', (req, res) {
+    res.json(noteData);
+});
+
+router.post('./api/notes', (req, res) => {
+    noteData.push(req.body);
+    res.json("A note was saved");
+});
+
+router.delete('./api/notes/:index', (req, res) => {
+    let del = parseInt(req.params.index);
+    let tempNotes = [];
+    for (let i = 0; i < noteData.length; i++) {
+        if (i !== del) {
+            tempNotes.push(noteData[i]);
+        }
+    }
+    noteData = tempNotes;
+
+    res.json("Note deleted!")
+});
+
+module.exports = router;
