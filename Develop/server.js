ec9629146f761925df0e4089a82b5f66ed07aac2
Dakota <piercedakota26@gmail.com>
2021-03-08 18:44:08 -0600

    added basic server file with comments

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,18 @@
+// Required Dependencies
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Starts the express app and ports the server to either a set port
+// or to default to 80
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 
 
 
-
+// Listens for setup
 app.listen(3001, () => {
-    console.log(`API server now on port 3001`)
+    console.log(`API server now on port ${PORT}`)
 });
